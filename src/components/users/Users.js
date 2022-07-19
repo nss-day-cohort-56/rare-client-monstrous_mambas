60fdb5d190a69171de7d3f7ec20ea068d759ca75
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-07-19 15:25:22 -0500

    added a user management navbar link that directs to users

---
+++ src/components/users/Users.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+
+
+export const Users = () => {
+
+    return ( <> 
+    <div>"Hello I'll be a user List someday"</div> </>) 
+}
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
 import { Authorized } from "./Authorized"
+import { Users } from "../components/users/Users"
 
 export const ApplicationViews = ({ token, setToken }) => {
   return <>
     <Routes>
       <Route path="/login" element={<Login setToken={setToken} />}  />
       <Route path="/register" element={<Register setToken={setToken} />}  />
+      <Route path="/users" element={<Users  />} />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
         
