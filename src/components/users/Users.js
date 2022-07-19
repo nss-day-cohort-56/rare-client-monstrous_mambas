1e3dcdeaf96f074b10f3774caf1b028bbe42bc82
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-07-19 15:33:13 -0500

    added some notes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,7 +4,8 @@ import { Link, useNavigate } from "react-router-dom"
 
 
 export const Users = () => {
-
+// we are going to display a list of users here
+//the list will contain their username, first and last name, and email.  they will be ordered by username
     return ( <> 
     <div>"Hello I'll be a user List someday"</div> </>) 
 }
