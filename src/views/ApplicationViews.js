7c05be877681aaeb9187b361d59532b452fbdaab
Camille Faulkner <camillecfaulkner@gmail.com>
2022-07-20 11:13:49 -0500

    view tags

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
+import { TagList } from "../components/tags/Tags"
 import { Authorized } from "./Authorized"
 
 export const ApplicationViews = ({ token, setToken }) => {
@@ -10,6 +11,7 @@ export const ApplicationViews = ({ token, setToken }) => {
       <Route path="/register" element={<Register setToken={setToken} />}  />
       <Route element={<Authorized token={token} />}>
         {/* Add Routes here */}
+      <Route path="/tags" element={<TagList />}  />
         
       </Route>
     </Routes>
