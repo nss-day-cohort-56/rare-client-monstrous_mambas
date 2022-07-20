7f5f370012291d5db22338463ed18ed523f02941
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-07-20 14:55:02 -0500

    created new category form that updates category list on submit

--- src/components/categories/CategoryManager.js
+++ src/components/categories/CategoryManager.js
@@ -2,3 +2,14 @@ export const getAllCategories= () => {
   return fetch("http://localhost:8088/categories")
   .then(res => res.json())
 };
+
+export const CreateNewCategory = (newCategory) => {
+  return fetch("http://localhost:8088/categories", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newCategory)
+  })
+      .then(res => res.json())
+}
