67ad79da391fe0ddbf2d0f421703345c4fe30911
Sydney Dickson <sydneyrachelledickson@gmail.com>
2022-07-20 13:57:21 -0500

    user details now displays details

--- src/components/users/UserDetails.js
+++ src/components/users/UserDetails.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
-import {getUserById} from "./UserManager"
+import { getUserById } from "./UserManager"
 
 export const UserDetails = () => {
     const { userId } = useParams()
@@ -11,5 +11,12 @@ export const UserDetails = () => {
             userData => setUser(userData))
     }, [])
 
-return (<>there will be user details here</>)///check and see what exactly needs to be displayed here, may need to update the get as well if joins are necessary
+return (<><div className="user-details">there will be user details here {user?.first_name} {user?.last_name}
+        {user?.profile_image_url ? 
+        <div><img src={user?.profile_image_url} alt="profile-pic"></img></div>
+        : "" } 
+        <div>{user?.username}</div>
+        <div>{user?.creation_date}</div>
+        <div>{user?.bio}</div>
+    </div></>)///check and see what exactly needs to be displayed here, may need to update the get as well if joins are necessary
 }
