6a9ad621ec3be3beddcbe42f6e92aedcf33b2da8
Camille Faulkner <camillecfaulkner@gmail.com>
2022-07-20 15:53:11 -0500

    you can delete a post

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom"
+import { deletePost } from "./PostManager"
 
-export const Post = ({id, title, name, category}) => {
+export const Post = ({ id, title, name, category, retrievePosts, count }) => {
+
+    const deleteButton = () => {
+        return <button onClick={(evt) => {
+            evt.preventDefault()
+            fetch(`http://localhost:8088/posts/${id}`, {
+                method: "DELETE"
+            })
+                .then(fetch(`http://localhost:8088/posts`)
+                    .then(response => response.json())
+                    .then((postsArray) => {
+                        retrievePosts(postsArray)
+                    }))
+                .then(count(true))
+        }}>Delete</button>
+    }
+
+
     return <section className="tile is-parent box has-background-white-ter" >
-  
-    <div className="tile is-child">
-        <Link className="has-text-grey-dark" to={`/post_details/${id}`} >{title}</Link>
+
+        <div className="tile is-child">
+            <Link className="has-text-grey-dark" to={`/post_details/${id}`} >{title}</Link>
         </div>
-    <div className="tile is-child"> {name}</div>
-    <div className="tile is-child"> {category}</div>
+        <div className="tile is-child"> {name}</div>
+        <div className="tile is-child"> {category}</div>
+        {deleteButton()}
 
-
-</section>
+    </section>
 }
--- src/components/posts/postList.js
+++ src/components/posts/postList.js
@@ -2,22 +2,47 @@ import { useEffect, useState } from "react"
 import { Post } from "./post"
 
 
-export const PostList = () => {
+export const PostList = ({ }) => {
     const [posts, setPosts] = useState([])
+    const [count, setCount] = useState(false)
+
+    // const retrievePosts = () => {
+    //     fetch(`http://localhost:8088/posts`)
+    //         .then(response => response.json())
+    //         .then((postsArray) => {
+    //             setPosts(postsArray)
+    //         })
+    // }
+
     useEffect(
         () => {
             fetch(`http://localhost:8088/posts`)
-                .then(response => response.json())
-                .then((postsArray) => {
-                    setPosts(postsArray)
-                })
-        },[] // When this array is empty,   
+            .then(response => response.json())
+            .then((postsArray) => {
+                setPosts(postsArray)
+                setCount(false)
+            })
+        }, [] 
     )
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/posts`)
+            .then(response => response.json())
+            .then((postsArray) => {
+                setPosts(postsArray)
+                setCount(false)
+            })
+
+        }, [count] 
+    )
+
+
     return <article className="has-background-white-ter pt-4 pr-5 pl-5">
-        
+
 
         {
-            posts.map(post => <Post key={` all_post--${post.id}`} id={post.id}  name={post?.user?.first_name} title ={post.title} category ={post?.category?.label}/>)
+            posts.map(post => <Post key={` all_post--${post.id}`} id={post.id} name={post?.user?.first_name} title={post.title} category={post?.category?.label} retrievePosts={setPosts} count={setCount}/>)
         }
-        </article>
+    </article>
 }
