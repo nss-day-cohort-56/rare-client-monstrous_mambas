3ecd19a611d61dfba50d5426777094190c50dae2
Camille Faulkner <camillecfaulkner@gmail.com>
2022-07-20 13:45:09 -0500

    steve fixed a weird react error

--- src/components/tags/TagForm.js
+++ src/components/tags/TagForm.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
-import { saveNewTag } from "./TagManager.js"
+import { saveNewTag } from "./TagManager"
+
 
 export const TagForm = ({retrieveTags}) => {
 
