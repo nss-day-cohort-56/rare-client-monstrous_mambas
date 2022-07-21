cac5a01757e771a89ea716c03bd6dbe90fe71dfc
Camille Faulkner <camillecfaulkner@gmail.com>
2022-07-21 15:48:27 -0500

    you can see and delete your own posts

--- src/components/posts/postList.js
+++ src/components/posts/postList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Post } from "./Post"
+import { Post } from "./post"
 
 
 export const PostList = ({ }) => {
--- src/managers/PostManager.js
+++ src/managers/PostManager.js
@@ -1,1 +1,17 @@
 // handel all fetch calls for post 
+
+export const getAllPosts = () => {
+    return fetch("http://localhost:8088/posts")
+    .then(res => res.json())
+};
+
+export const saveNewPost = (post) => {
+    return fetch("http://localhost:8088/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(post)
+    })
+        .then(res => res.json())
+}
