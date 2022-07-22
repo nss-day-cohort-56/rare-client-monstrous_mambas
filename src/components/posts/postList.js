f89b5296c58fe635e7885c82ff1e262a1f1bcdbf
Claire Morgan-Sanders <crmo2444@gmail.com>
2022-07-22 14:57:11 -0500

    merge commit

--- src/components/posts/postList.js
+++ src/components/posts/postList.js
@@ -13,6 +13,16 @@ export const PostList = () => {
         []
     )
     return <article className="has-background-white-ter pt-4 pr-5 pl-5">
+           <select className="gameFilter" onChange={(event) => {
+                            let chosenPlatform = event.target.value
+                            setChosen(parseInt(chosenPlatform))
+                        }}>
+        <option value="0">Filter by...</option>
+        {platforms.map(platform => {
+            count++
+            return <option value={`${platform.id}`}>{platform.name}</option>
+        })}
+    </select>
         {
             posts.map(post => <Post key={` all_post--${post.id}`} id={post.id} name={post?.user?.first_name} title={post.title} category={post?.category?.label} />)
         }
