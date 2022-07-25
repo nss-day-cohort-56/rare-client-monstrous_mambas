0b8d69ad70616afc00cd7da832cfb2a8999c0e26
Camille Faulkner <camillecfaulkner@gmail.com>
2022-07-25 11:56:16 -0500

    commit

--- src/components/comments/CommentManager.js
+++ src/components/comments/CommentManager.js
@@ -2,3 +2,10 @@ export const getCommentsByPostId = (id) => {
     return fetch(`http://localhost:8088/comments?post_id=${id}`)
     .then(res => res.json())
 };
+
+
+export const deleteComment = (commentId) => {
+    return fetch(`http://localhost:8088/comments/${commentId}`, {
+        method: "DELETE"
+    })
+};
